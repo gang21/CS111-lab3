70ddc4a3c43eecbb492d408a2e9d8bf5854df2ff
Gabriella Ang <gabriella90631@gmail.com>
2023-05-10 12:41:05 -0700

    v1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Hash Hash Hash
-This lab xxx
+This lab creates a hash table implementation that is safe to use concurrently. 
 
 ## Building
 ```shell
@@ -16,6 +16,11 @@ In the `hash_table_v1_add_entry` function, xxx
 
 ### Performance
 ```shell
+Generation: 72,600 usec
+Hash table base: 362,071 usec
+  - 0 missing
+Hash table v1: 703,576 usec
+  - 0 missing
 
 ```
 
--- hash-table-v1.c
+++ hash-table-v1.c
@@ -72,6 +72,9 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
                              const char *key,
                              uint32_t value)
 {
+	//ADD LOCK HERE
+	static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_lock(&mutex);
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
@@ -86,6 +89,9 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+	//UNLOCK HERE
+	pthread_mutex_unlock(&mutex);
+	pthread_mutex_destroy(&mutex);
 }
 
 uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
